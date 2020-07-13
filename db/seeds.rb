ed1b4d1823fa9795249456187f067944f4d6e130
Brodie Hunt <brodiehunt7@gmail.com>
2020-07-13 11:18:04 +1000

    database created, toy table populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..20
+  Toy.create(
+    name: Faker::FunnyName.name,
+    description: Faker::Quote.famous_last_words,
+    picture: "No Picture",
+    date_posted: Faker::Date.between(from: '2020-01-01', to: '2020-07-13'),
+    user: Faker::Name.name
+  )
+  puts "Created #{i} records"
+end
