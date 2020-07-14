3ca9b22a6b45325e01245b666fdf112bb597d8db
Brodie Hunt <brodiehunt7@gmail.com>
2020-07-14 19:43:31 +1000

    Full CRUD functionality

---
+++ app/controllers/toys_controller.rb
@@ -0,0 +1,42 @@
+class ToysController < ApplicationController
+  
+  def index
+    puts "working"
+    @toys = Toy.all 
+  end
+
+  def create
+    puts params
+    Toy.create(name: params[:toy][:name], description: params[:toy][:description], picture: params[:toy][:picture],
+                date_posted: params[:toy][:date_posted], user: params[:toy][:user])
+    redirect_to toy_path(Toy.maximum(:id))
+  end
+  
+  def new
+  end
+
+  def show
+    @toy = Toy.find(params[:id].to_i)
+    p params
+  end
+
+  def edit
+    puts "Is my controller even working"
+    @toy = Toy.find(params[:id].to_i)
+  end
+
+  def update
+    puts params
+    @toy = Toy.find(params[:id].to_i)
+    @toy.update(name: params[:toy][:name], description: params[:toy][:description], picture: params[:toy][:picture],
+      date_posted: params[:toy][:date_posted], user: params[:toy][:user])
+    redirect_to toy_path(params[:id].to_i)
+  end
+
+  def destroy
+    @toy = Toy.find(params[:id].to_i)
+    @toy.destroy
+    redirect_to toys_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/toys", to: "toys#index", as: "toys"
+  post "/toys", to: "toys#create"
+  get "/toys/new", to: "toys#new", as: "new_toy"
+  get "/toys/:id", to: "toys#show", as: "toy"
+  get "/toys/:id/edit", to: "toys#edit", as: "edit_toy"
+  patch "/toys/:id", to: "toys#update"
+  delete "/toys/:id", to: "toys#destroy"
 end
